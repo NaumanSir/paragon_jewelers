2dd8f0e819c407c47ba7d6ab63fcd52585f11037
Nauman Husnani <naumanhusnani@gmail.com>
2020-01-28 17:26:02 -0800

    props are being mapped and passed as expected, but are still on the wrong page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,27 +1,21 @@
 import React from "react"
 
-// class Browse extends React.Component {
-//     // constructor() {
-//     //     super()
-//     //     this.state = {}
-//     // }
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Browse</h1>
-//                 <h4>{this.props.id}</h4>
-//             </div>
-//         )
-//     }
-// }
-
-function Browse(props) {
-    return (
-        <div>
-             <h1>Browse</h1>
-             <h4>{props.product.id}</h4>
-         </div>
-    )
+class Browse extends React.Component {
+    // constructor() {
+    //     super()
+    //     this.state = {}
+    // }
+    render() {
+        return (
+            <div>
+                <h1>Browse</h1>
+                <h4>{this.props.product.id}</h4>
+                <h4>{this.props.product.name}</h4>
+                <img src={this.props.product.img_src} />
+                <h4>{this.props.product.price}</h4>
+            </div>
+        )
+    }
 }
 
 export default Browse
