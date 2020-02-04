0cda60665e07f7391c106f432210d8a6e66781f2
Nauman Husnani <naumanhusnani@gmail.com>
2020-02-04 12:46:29 -0800

    added maps dependency

--- src/components/Visit.js
+++ src/components/Visit.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Visit.css"
 
 class Visit extends React.Component {
     // constructor() {
@@ -7,8 +8,15 @@ class Visit extends React.Component {
     // }
     render() {
         return (
-            <div>
-                <h1>Visit Us</h1>
+            <div className="visit">
+                <h4>Our Address:</h4>
+                <p>1221 N Rodeo Drive</p>
+                <p>Beverly Hills, CA 90210</p>
+                <div className="call_for_dir">
+                    <h4>Need Directions?</h4>
+                    <p>Call us at (310) 555-0126</p>
+                    <p>We're more than happy to help.</p>
+                </div>
             </div>
         )
     }
