e2e344d7d39e013c94cabbbe915a2ecb72ae8e3f
Nauman Husnani <naumanhusnani@gmail.com>
2020-02-04 13:34:05 -0800

    Added Google map. Excluded api key for this commit.

--- src/components/Visit.js
+++ src/components/Visit.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Visit.css"
+import { Map, GoogleApiWrapper } from "google-maps-react"
 
 class Visit extends React.Component {
     // constructor() {
@@ -9,12 +10,17 @@ class Visit extends React.Component {
     render() {
         return (
             <div className="visit">
+                <Map
+                    google={this.props.google}
+                    zoom={8}
+                    initialCenter={{ lat: 34.067210, lng: -118.401050 }}
+                />
                 <h4>Our Address:</h4>
                 <p>1221 N Rodeo Drive</p>
                 <p>Beverly Hills, CA 90210</p>
                 <div className="call_for_dir">
                     <h4>Need Directions?</h4>
-                    <p>Call us at (310) 555-0126</p>
+                    <p>Call us at (310) 555-0126.</p>
                     <p>We're more than happy to help.</p>
                 </div>
             </div>
@@ -22,4 +28,6 @@ class Visit extends React.Component {
     }
 }
 
-export default Visit
+export default GoogleApiWrapper({
+    apiKey: ''
+})(Visit)
