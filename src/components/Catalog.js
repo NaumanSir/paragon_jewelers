272291c356a76b55e8441c842364febe3d652211
Nauman Husnani <naumanhusnani@gmail.com>
2020-02-08 12:36:54 -0800

    added some matching css

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,11 +1,15 @@
 import React from "react"
+import "./Catalog.css"
 import paragon_catalog from "./paragon_catalog.pdf"
 
 class Catalog extends React.Component {
     render() {
         return (
             <div>
-                <a href={paragon_catalog}>Click Here</a>
+                <div className="download">
+                    <a href={paragon_catalog}><button>Download our Spring 2020 catalog</button></a>
+                </div>
+                {/* <p className="call_us">Have any questions? Call us at (310) 555-0126.</p> */}
             </div>
         )
     }
