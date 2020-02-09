bebfb3e9806964202ebd3aebd24483934b213a50
Nauman Husnani <naumanhusnani@gmail.com>
2020-02-08 17:16:32 -0800

    content added to 'Contact Us' section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,30 @@
 import React from "react"
+import "./Contact.css"
+import Visit from "./Visit"
 
 class Contact extends React.Component {
-    // constructor() {
-    //     super()
-    //     this.state = {}
-    // }
+    constructor() {
+        super()
+        this.state = {
+            render: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({ render: true })
+    }
+
     render() {
         return (
-            <div>
+            <div className="contact_section">
                 <h1>Contact Us</h1>
+                <p>We're here to help with whatever your jewelry needs may be. Don't hesitate to contact us.</p>
+                <p><strong>Email us:</strong> paragon@paragonjewelers.com</p>
+                <p><strong>Call us:</strong> (310) 555-0126</p>
+                {/* <img src="https://cdn.pixabay.com/photo/2014/10/24/03/22/rodeo-500716_1280.jpg" /> */}
+                <button onClick={this.handleClick}>Visit Us</button>
+                {this.state.render ? <Visit /> : null}
             </div>
         )
     }
--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Customize.css"
 
 class Customize extends React.Component {
     // constructor() {
@@ -7,30 +8,31 @@ class Customize extends React.Component {
     // }
     render() {
         return (
-            <div>
+            <div className="customize">
                 <div className="form_header">
                     <h1>Request an order of customized jewelry from Paragon Jewelers.</h1>
                     <p>Complete the form below and Paragon Jewelers will contact you to clarify details and get you a quote.</p>
                 </div>
                 <div className="form_itself">
                     <form>
-                        Your full name:
+                        Your full name:<br />
                         <input type="text" name="fullname" value="full name" /><br />
                         Is this for a movie studio?
                         <input type="radio" name="for_movie_studio" value="yes" />Yes
                         <input type="radio" name="for_movie_studio" value="no" />No<br />
-                        Company / Studio name (optional):
+                        Company / Studio name (optional):<br />
                         <input type="text" name="studio_name" value="company / studio" /><br />
-                        Does your request fall into any of the following categories?
+                        Does your request fall into any of the following categories?<br />
                         <select>
                             <option value="ring">Ring</option>
                             <option value="necklace">Necklace</option>
                             <option value="bracelet">Bracelet / Bangle</option>
+                            <option value="none">None</option>
                         </select><br />
                         Number of pieces:
                         <input type="number" name="no_of_pieces" /><br />
                         Describe what you imagine the custom piece(s) to look like<br />
-                        <input type="textarea" name="desc" /><br />
+                        <input type="textarea" cols="50" name="desc" /><br />
                         Is there any custom functionality you would like in this piece (fold out, light up, hidden compartment, etc.)?<br />
                         <input type="textarea" name="functionality_desc" /><br />
                         <input type="submit" value="Submit" />
